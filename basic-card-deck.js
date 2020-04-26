83fdda831a904c112940631fa844b2ab9fe777b6
zhanknight <zhanknight@gmail.com>
2020-04-26 12:23:34 -0400

    Implemented deal & deal multiple methods

--- basic-card-deck.js
+++ basic-card-deck.js
@@ -1,6 +1,12 @@
 // Trying to model a basic deck of cards using JS
 // Practicing OOP in JS
 
+//TODO
+// add argument validation on makeDeck method
+// add argument validation to dealCards method
+// implement splitDeck method
+// create a subclass for non-standard deck configurations (no face cards, etc)
+
 
 // Each card should be an object, so let's make a class
 class Card {
@@ -46,8 +52,32 @@ class Deck {
         for (let i=0; i<m; i++) {
             let j = this.cards.shift();
             this.cards.push(j);
+            console.log(i, j);
         }
         console.log('Deck has been cut!')
+    }
+    // this method splits the deck object into two objects (deckname1/deckname2)
+    splitDeck() {
+        console.log('Split deck method is not implemented yet.')
+        // get name of current deck    
+        // create two new Deck instances using current deck name, append 1 & 2
+        // take top half of current deck, push into currentdeck1, other half into currentdeck2
+        // destroy current deck?
+        // Am I gonna have scoping/persistence issues on this?
+    }
+    //this method returns the 'top' card from the deck
+    dealCard() {
+        console.log('Card dealt!');
+        return this.cards.shift();
+    }
+    //this method returns the specified number of cards from the 'top' as an array of card objects
+    dealCards(num) {
+        console.log(`${num} cards dealt!`);
+        let dealtCards = [];
+        for (let i=num; i>0; i--) {
+            dealtCards.push(this.cards.shift());
+        }
+        return dealtCards;
     }
 }
 
@@ -78,3 +108,9 @@ myDeck.makeDeck();
 myDeck.shuffleDeck();
 myDeck.cutDeck();
 showDeck();
+
+
+// cribbage
+// blackjack
+// poker
+// progression
